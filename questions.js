d0de755a1aec87796eda1c7c8391a792a462e7cf
sanjivani3 <137819545+sanjivani3@users.noreply.github.com>
2023-09-05 20:56:22 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Who Played Chris Gardner in the movie 'The Pursuit of Happyness'?",
+    choice1: "Chris Evans",
+    choice2: "Samuel L. Jackson",
+    choice3: "Will Smith",
+    choice4: "Tom Hanks",
+    answer: 2
+},
+            {
+            question: "Which is the No. 01 collection movie in world?",
+    choice1: "Avatar",
+    choice2: "Avengers: Endgame",
+    choice3: "Avatar: The Way of Water",
+    choice4: "Titanic",
+    answer: 0
+},
+            {
+            question: "Who is main villen in the movie 'Spiderman: No Way Home'?",
+    choice1: "Electro",
+    choice2: "Green Goblin",
+    choice3: "Venom",
+    choice4: "Doctor Octopus",
+    answer: 1
+},
+            {
+            question: "What is kill count of John Wick in the movie 'John Wick: Chapter 3'?",
+    choice1: "108",
+    choice2: "67",
+    choice3: "147",
+    choice4: "94",
+    answer: 3
+},
+            {
+            question: "How many Oscars Nominations did the 1997 movie 'Titanic' got?",
+    choice1: "13",
+    choice2: "9",
+    choice3: "10",
+    choice4: "14",
+    answer: 3
+},
+            {
+            question: "In which form did 'The Jungle Book' originally appear?In which form did 'The Jungle Book' originally appear?",
+    choice1: "Radio Play",
+    choice2: "Film",
+    choice3: "Book",
+    choice4: "An app",
+    answer: 2
+},
+            {
+               question: "What is name of Emma Stone's character in movie 'La La Land'?",
+    choice1: "Mia Dolan",
+    choice2: "Gwen Stacy",
+    choice3: "Wichita",
+    choice4: "Hannah Clark",
+    answer: 0
+},{
+               question: "Who is the most oldest superhero in the comics?",
+    choice1: "Superman",
+    choice2: "The Phantom",
+    choice3: "Mrs. Marvel",
+    choice4: "Batman",
+    answer: 1
+},
+            {
+               question: "Who is the director of movie 'Dangal'?",
+    choice1: "Karan Johar",
+    choice2: "Zoya Akhtar",
+    choice3: "Nitish Tiwari",
+    choice4: "S. S. Rajamouli",
+    answer: 2
+},
+            {
+               question: "What was the first movie based on a book?",
+    choice1: "The Godfather",
+    choice2: "The Wolf of Wall Street",
+    choice3: "Forrest Gump",
+    choice4: "Cinderella",
+    answer: 3
+}];
